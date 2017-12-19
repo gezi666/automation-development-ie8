68d34c8724fd05a247bdc667c78c0a8143c70adc
yyg <yygwork@126.com>
2017-12-19 17:49:56 +0800

    修改gulp模板

--- build/tasks/scripts.js
+++ build/tasks/scripts.js
@@ -5,9 +5,10 @@ module.exports = function(gulp, plugins, options) {
             .pipe(plugins.plumber())
             .pipe(plugins.if(options.env === 'production', plugins.rev())) //set hash key
             .pipe(plugins.changed('./dist/js'))
-            .pipe(plugins.babel({
-                presets: ['env']
-            }))
+            // .pipe(plugins.babel({
+            //  使用中发现此处把jquery进行格式化，导致js报错
+            //     presets: ['env']
+            // }))
             .pipe(gulp.dest("dist/js"))
             .pipe(plugins.if(options.env === 'production', plugins.rev.manifest()))
             .pipe(plugins.if(options.env === 'production', gulp.dest('rev/js')))
